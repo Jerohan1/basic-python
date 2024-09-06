e83989367b165a8fdbbdf3bbd249723160a69ad0
Jerohan1 <95771724+Jerohan1@users.noreply.github.com>
2024-09-06 11:49:51 +0200

    pass and counts

--- src/password.py
+++ src/password.py
@@ -8,5 +8,17 @@ password = sys.argv[1]
 is_valid = False
 
 # Do all the requirement checks here.
-
-print(is_valid)
+checks = [False, False, False, False, False]
+for c in password:
+    if c.isupper():
+        checks[0] = True
+    if c.islower():
+        checks[1] = True
+    if c.isnumeric():
+        checks[2] = True
+    if c in "$#@":
+        checks[3] = True
+    if len(password) < 17 and len(password) >= 6:
+        checks[4] = True
+if checks.count(True) == 5:
+    print(is_valid)
