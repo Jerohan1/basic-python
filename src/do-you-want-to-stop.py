cad4b24d272d94e0726ca6f16203015a7023966f
Jerohan1 <95771724+Jerohan1@users.noreply.github.com>
2024-09-06 10:36:50 +0200

    stop?

--- src/do-you-want-to-stop.py
+++ src/do-you-want-to-stop.py
@@ -1,3 +1,6 @@
 
 # You should write a loop around this question, and keep asking until
 # the answer is "yes" (lower case).
+ans = ''
+while(ans != 'yes'):
+    ans = input('Do you want to stop? ')
